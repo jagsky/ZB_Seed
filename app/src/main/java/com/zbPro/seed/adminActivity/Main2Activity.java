48ff0295fcc31ccc5cd58780ca9587d4a2c3f4ff
Administrator <www.275139314@qq.com>
2016-07-27 13:40:58 +0800

    1

---
+++ app/src/main/java/com/zbPro/seed/adminActivity/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.zbPro.seed.adminActivity;
+
+import android.os.Bundle;
+
+import com.zbPro.seed.activity.BaseActivity;
+import com.zbPro.seed.activity.R;
+
+public class Main2Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
