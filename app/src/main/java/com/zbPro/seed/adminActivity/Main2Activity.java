06cb9b01a3cd08e0a82f2e37b7819e51e4f3db68
Administrator <www.275139314@qq.com>
2016-07-27 13:56:29 +0800

    1

--- app/src/main/java/com/zbPro/seed/adminActivity/Main2Activity.java
+++ app/src/main/java/com/zbPro/seed/adminActivity/Main2Activity.java
@@ -1,15 +1,23 @@
 package com.zbPro.seed.adminActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.zbPro.seed.activity.BaseActivity;
 import com.zbPro.seed.activity.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class Main2Activity extends BaseActivity {
+
+    @Bind(R.id.admin_mian2_listView)
+    ListView adminMian2ListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ButterKnife.bind(this);
     }
 }
