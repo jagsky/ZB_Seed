feeb6096e911a5ab65125d3de245f8c86e830387
Administrator <www.275139314@qq.com>
2016-07-28 13:56:28 +0800

    1

--- app/src/main/java/com/zbPro/seed/adminActivity/Main2Activity.java
+++ app/src/main/java/com/zbPro/seed/adminActivity/Main2Activity.java
@@ -1,24 +1,43 @@
 package com.zbPro.seed.adminActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.zbPro.seed.activity.BaseActivity;
 import com.zbPro.seed.activity.R;
+import com.zbPro.seed.view.RefreshableView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class Main2Activity extends BaseActivity {
 
-    @Bind(R.id.admin_mian2_listView)
-    ListView adminMian2ListView;
+
+    @Bind(R.id.list_view)
+    ListView listView;
+    @Bind(R.id.refreshable_view)
+    RefreshableView refreshableView;
+    ArrayAdapter<String> adapter;
+    String[] items = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         ButterKnife.bind(this);
-
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+        refreshableView.setOnRefreshListener(new RefreshableView.PullToRefreshListener() {
+            @Override
+            public void onRefresh() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                refreshableView.finishRefreshing();
+            }
+        }, 0);
     }
 }
