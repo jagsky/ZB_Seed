cfa344c17557b8980ed10129a8b0e46399eeebf7
Administrator <www.275139314@qq.com>
2016-08-05 13:39:09 +0800

    1

--- app/src/main/java/com/zbPro/seed/adminActivity/Admin_GainActitivy.java
+++ app/src/main/java/com/zbPro/seed/adminActivity/Admin_GainActitivy.java
@@ -1,6 +1,5 @@
 package com.zbPro.seed.adminActivity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.zbPro.seed.activity.BaseActivity;
@@ -15,5 +14,6 @@ public class Admin_GainActitivy extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__gain_actitivy);
+
     }
 }
