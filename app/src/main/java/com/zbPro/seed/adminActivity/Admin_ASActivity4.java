af09aa83caacf5d8b0c025c6ff8ba6c40208b736
Administrator <www.275139314@qq.com>
2016-08-12 14:03:03 +0800

    2016/08/11

---
+++ app/src/main/java/com/zbPro/seed/adminActivity/Admin_ASActivity4.java
@@ -0,0 +1,15 @@
+package com.zbPro.seed.adminActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zbPro.seed.activity.R;
+
+public class Admin_ASActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin__as4);
+    }
+}
