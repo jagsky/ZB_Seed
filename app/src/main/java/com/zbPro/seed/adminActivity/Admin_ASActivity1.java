0999acc50790a6391aa40d42c589d9eeeecabc89
Administrator <www.275139314@qq.com>
2016-08-15 15:39:29 +0800

    2016/08/11

--- app/src/main/java/com/zbPro/seed/adminActivity/Admin_ASActivity1.java
+++ app/src/main/java/com/zbPro/seed/adminActivity/Admin_ASActivity1.java
@@ -1,9 +1,15 @@
 package com.zbPro.seed.adminActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.zbPro.seed.activity.BaseActivity;
 import com.zbPro.seed.activity.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 /*
 *作用：年度调查界面
 *时间：2016.08.05
@@ -11,11 +17,34 @@ import com.zbPro.seed.activity.R;
 
 public class Admin_ASActivity1 extends BaseActivity {
 
+    @Bind(R.id.admin_as_ed1)
+    EditText adminAsEd1;
+    @Bind(R.id.admin_as_ed2)
+    EditText adminAsEd2;
+    @Bind(R.id.admin_as_ed3)
+    EditText adminAsEd3;
+    @Bind(R.id.admin_as_ed4)
+    EditText adminAsEd4;
+    @Bind(R.id.admin_as_ed5)
+    EditText adminAsEd5;
+    @Bind(R.id.admin_as_ed6)
+    EditText adminAsEd6;
+    @Bind(R.id.admin_as_ed7)
+    EditText adminAsEd7;
+    @Bind(R.id.admin_as1_btn)
+    Button adminAs1Btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__as);
+        ButterKnife.bind(this);
 
 
+    }
+
+    @OnClick(R.id.admin_as1_btn)
+    public void onClick() {
+        adminAsEd1.getText().toString();
     }
 }
--- app/src/main/java/com/zbPro/seed/adminActivity/Admin_RogueActivity.java
+++ app/src/main/java/com/zbPro/seed/adminActivity/Admin_RogueActivity.java
@@ -1,18 +1,159 @@
 package com.zbPro.seed.adminActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.bigkoo.quicksidebar.QuickSideBarTipsView;
+import com.bigkoo.quicksidebar.QuickSideBarView;
+import com.bigkoo.quicksidebar.listener.OnQuickSideBarTouchListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersAdapter;
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersDecoration;
 import com.zbPro.seed.activity.BaseActivity;
 import com.zbPro.seed.activity.R;
+import com.zbPro.seed.adapter.CityListAdapter;
+import com.zbPro.seed.adapter.DividerDecoration;
+import com.zbPro.seed.bean.City;
+import com.zbPro.seed.dao.CityDao;
+import com.zbPro.seed.util.DataConstants;
 
-/*作用：管理员 去杂界面 查询
-* 时间：2016.8.4
-* */
-public class Admin_RogueActivity extends BaseActivity {
+import java.lang.reflect.Type;
+import java.security.SecureRandom;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Admin_RogueActivity extends BaseActivity implements OnQuickSideBarTouchListener {
+    List<City> cities;
+    City city = new City();
+    CityDao cityDao =new CityDao(this);
+    RecyclerView recyclerView;
+    HashMap<String,Integer> letters = new HashMap<>();
+    QuickSideBarView quickSideBarView;
+    QuickSideBarTipsView quickSideBarTipsView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__rogue);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        quickSideBarView = (QuickSideBarView) findViewById(R.id.quickSideBarView);
+        quickSideBarTipsView = (QuickSideBarTipsView) findViewById(R.id.quickSideBarTipsView);
+
+        //设置监听
+        quickSideBarView.setOnQuickSideBarTouchListener(this);
+
+
+        //设置列表数据和浮动header
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Add the sticky headers decoration
+        CityListWithHeadersAdapter adapter = new CityListWithHeadersAdapter();
+
+        //GSON解释出来
+        Type listType = new TypeToken<LinkedList<City>>(){}.getType();
+        Gson gson = new Gson();
+
+        try {
+            cities= cityDao.queryAllCity();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        //    LinkedList<City> cities = gson.fromJson(cities1, listType);
+
+        ArrayList<String> customLetters = new ArrayList<>();
+
+        int position = 0;
+        for(City city: cities){
+            String letter = city.getFirstLetter();
+            //如果没有这个key则加入并把位置也加入
+            if(!letters.containsKey(letter)){
+                letters.put(letter,position);
+                customLetters.add(letter);
+            }
+            position++;
+        }
+
+        //不自定义则默认26个字母
+        quickSideBarView.setLetters(customLetters);
+        adapter.addAll(cities);
+        recyclerView.setAdapter(adapter);
+
+        final StickyRecyclerHeadersDecoration headersDecor = new StickyRecyclerHeadersDecoration(adapter);
+        recyclerView.addItemDecoration(headersDecor);
+
+        // Add decoration for dividers between list items
+        recyclerView.addItemDecoration(new DividerDecoration(this));
+    }
+
+
+    @Override
+    public void onLetterChanged(String letter, int position, float y) {
+        quickSideBarTipsView.setText(letter, position, y);
+        //有此key则获取位置并滚动到该位置
+        if(letters.containsKey(letter)) {
+            recyclerView.scrollToPosition(letters.get(letter));
+        }
+    }
+
+    @Override
+    public void onLetterTouching(boolean touching) {
+        //可以自己加入动画效果渐显渐隐
+        quickSideBarTipsView.setVisibility(touching? View.VISIBLE:View.INVISIBLE);
+    }
+
+    private class CityListWithHeadersAdapter extends CityListAdapter<RecyclerView.ViewHolder>
+            implements StickyRecyclerHeadersAdapter<RecyclerView.ViewHolder> {
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.view_item, parent, false);
+            return new RecyclerView.ViewHolder(view) {
+            };
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            TextView textView = (TextView) holder.itemView;
+            textView.setText(getItem(position).getCityName());
+        }
+
+        @Override
+        public long getHeaderId(int position) {
+            return getItem(position).getFirstLetter().charAt(0);
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.view_header, parent, false);
+            return new RecyclerView.ViewHolder(view) {
+            };
+        }
+
+        @Override
+        public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
+            TextView textView = (TextView) holder.itemView;
+            textView.setText(String.valueOf(getItem(position).getFirstLetter()));
+            holder.itemView.setBackgroundColor(getRandomColor());
+        }
+
+        private int getRandomColor() {
+            SecureRandom rgen = new SecureRandom();
+            return Color.HSVToColor(150, new float[]{
+                    rgen.nextInt(359), 1, 1
+            });
+        }
+
     }
 }
